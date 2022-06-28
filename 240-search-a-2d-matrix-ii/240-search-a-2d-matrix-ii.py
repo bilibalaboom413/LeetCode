4a5e8d4b44c7735c49f1fabba0b4fde30d053b42
Hao Chen <hche8093@uni.sydney.edu.au>
2022-06-28 20:21:56 +0800

    Time: 161 ms (71.64%), Space: 19.6 MB (20.00%) - LeetHub

---
+++ 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def searchMatrix(self, matrix, target):
+        """
+        :type matrix: List[List[int]]
+        :type target: int
+        :rtype: bool
+        """
+        
+        m, n = len(matrix), len(matrix[0])
+        
+        i, j = 0, n - 1
+        
+        while i < m and j >= 0:
+            if matrix[i][j] == target:
+                return True
+            elif matrix[i][j] < target:
+                i += 1
+            elif matrix[i][j] > target:
+                j -= 1
+        
+        return False
+            
+        
+        
